6da51dab25601b5c08ec6909e3b033806b04a8e0
Nikita Kapadiya <nikikapadiya97@gmail.com>
2025-12-24 18:01:38 +0530

    changes

--- booking/views.py
+++ booking/views.py
@@ -238,13 +238,13 @@ class CandidateBookSlotView(APIView):
             [candidate.candidate_email],
         )
         round=None
-        if candidate.status == 'interview_pending_1':
+        if candidate.status == 'shortlisted':
             round = "hr_round"
-        if candidate.status == 'interview_pending_2':
+        if candidate.status == 'interview_next_2':
             round = "technical_round_1"
-        if candidate.status == 'interview_pending_3':
+        if candidate.status == 'interview_next_3':
             round = "technical_round_2"
-        if candidate.status == 'interview_pending_final':
+        if candidate.status == 'interview_next_final':
             round = "final_round"
         feedback_link = f"http://localhost:5173/api/slots/interview-feedback/?job_application={candidate.id}&interview_round={round}"
 
