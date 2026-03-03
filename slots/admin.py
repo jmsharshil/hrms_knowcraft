0e46ff5b627da73a7f628df78ddb66ee353f0370
AnandShah10 <shahannad1072004@gmail.com>
2026-03-03 12:43:52 +0530

    location added in admin panel

--- slots/admin.py
+++ slots/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Slot,Interviewer,InterviewFeedback
+from .models import Slot,Interviewer,InterviewFeedback,InterviewLocation
 # Register your models here.
 
 admin.site.register(Slot)
 admin.site.register(Interviewer)
 admin.site.register(InterviewFeedback)
+admin.site.register(InterviewLocation)
