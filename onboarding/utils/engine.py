331a77f5fb81ffa0006a333e877c3d97a62839dd
AnandShah10 <shahannad1072004@gmail.com>
2026-03-18 16:28:00 +0530

    round name after shortlisted

--- onboarding/utils/engine.py
+++ onboarding/utils/engine.py
@@ -181,12 +181,16 @@ def automation_engine(candidate, old, new):
     if new == 'shortlisted':
         if candidate.job.mrf.interviewer_email_1:
             interviewer_email = candidate.job.mrf.interviewer_email_1
+            candidate.round_name = "hr_round"
         elif candidate.job.mrf.interviewer_email_2:
             interviewer_email = candidate.job.mrf.interviewer_email_2
+            candidate.round_name = "technical_round"
         elif candidate.job.mrf.interviewer_email_3:
             interviewer_email = candidate.job.mrf.interviewer_email_3
+            candidate.round_name = "case_study_round"
         elif candidate.job.mrf.interviewer_email_final:
             interviewer_email = candidate.job.mrf.interviewer_email_final
+            candidate.round_name = "final_round"
     elif new == "interview_next_2":
         interviewer_email = candidate.job.mrf.interviewer_email_2
     elif new == "interview_next_3":
