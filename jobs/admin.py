3a202bb817990ca57ba5a2a2c5e35953bb3441d5
AnandShah10 <shahannad1072004@gmail.com>
2026-03-20 09:49:14 +0530

    fix

--- jobs/admin.py
+++ jobs/admin.py
@@ -62,7 +62,7 @@ class JobAdmin(admin.ModelAdmin):
     
     def get_queryset(self, request):
         return super().get_queryset(request).select_related(
-            'department', 'designation', 'mrf', 'assigned_consultancies',
+            'department', 'designation', 'mrf',
             'posted_by', 'company'
         )
 
