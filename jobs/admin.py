9d9f86203131f29ad78102cdc0e131c2581de9e7
AnandShah10 <shahannad1072004@gmail.com>
2026-03-20 09:55:29 +0530

    change

--- jobs/admin.py
+++ jobs/admin.py
@@ -45,7 +45,7 @@ class JobAdmin(admin.ModelAdmin):
         }),
         ('Assignment Details', {
             'fields': (
-                'assigned_consultancies', 'assigned_at', 'assigned_by'
+                'assigned_consultancies', 'assigned_internal_hrs', 'assigned_at', 'assigned_by'
             )
         }),
         # ('Closure Details', {
