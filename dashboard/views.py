3d60f2c85a0b4f80cb0c27a28b6f78c5324461c0
AnandShah10 <shahannad1072004@gmail.com>
2026-04-13 10:53:56 +0530

    UPDATE

--- dashboard/views.py
+++ dashboard/views.py
@@ -311,7 +311,8 @@ class BaseAnalyticsView(APIView):
                     Q(job__assigned_to_consultancy_id=user_id) |
                     Q(job__assigned_consultancies__id=user_id) |
                     Q(job__assigned_to_internal_hr_id=user_id) |
-                    Q(job__assigned_internal_hrs__id=user_id)
+                    Q(job__assigned_internal_hrs__id=user_id) |
+                    Q(job__mrf__requested_by_id=user_id)
                 )
         app_qs = JobApplication.objects.filter(app_filter).distinct()
         
@@ -1006,7 +1007,7 @@ class CandidateExperienceFeedbackSubmitView(APIView):
                 return Response({"detail": "No feedback has been given till now."}, status=status.HTTP_204_NO_CONTENT)
 
         except Exception as e:
-            return Response({"detail": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response({"detail": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
     def post(self, request):
         serializer = CandidateExperienceFeedbackSubmitSerializer(data=request.data)
